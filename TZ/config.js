b0c4d53f6e03a800e172fb9c440d3f4f859d4156
ThekingMX1998 <chen1263754478@gmail.com>
2022-10-04 01:26:53 +0800

    Delete TZ directory

--- TZ/config.js
+++
@@ -1,6 +0,0 @@
-window.__PRE_CONFIG__ = {
-  header: 'GreenFish Server Status',
-  subHeader: 'Servers\' Probes Set up with ServerStatus',
-  interval: 1.5,
-  footer: '<p>Powered by <a href="https://www.greenfishpowered.cn">GreenFish</a></p>'
-};
