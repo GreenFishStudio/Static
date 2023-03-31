737a439adb0a67611c83577f16807fbd57129d67
ThekingMX1998 <chen1263754478@gmail.com>
2023-04-01 06:58:59 +0800

    Update config.js

--- TZ/config.js
+++ TZ/config.js
@@ -2,5 +2,5 @@ window.__PRE_CONFIG__ = {
   header: 'GreenFish Server Status',
   subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
-  footer: '<p>Powered by <a href="https://www.greenfishpowered.cn">GreenFish</a></p>'
+  footer: '<p>Powered by <a href="https://www.betarise.me.uk">GreenFish</a></p>'
 };
